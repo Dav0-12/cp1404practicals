20bcd48769925a06134ebd6749b17804e5e54057
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2023-09-19 20:00:44 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,11 +2,20 @@
 
 
 def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
     min_password_length = int(input("Password length: "))
     password = input("Enter password: ")
     while len(password) < min_password_length:
         password = input("Enter password: ")
-    print("*" * len(password))
+    return password
 
 
 main()
