51bd0249fe84c390e97a9bbd73bbed71620e3896
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2023-09-26 15:27:24 +1000

    Add comments about random numbers and required function

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+"""Prac 3 Randoms - Questions Answers"""
+import random
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# We saw random integers between 5 and 20 inclusive
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# We saw a random integer between 3 and 10, incrementing by 2. Therefore, the only possible
+# random numbers from this function are 3, 5, 7, 9
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# We saw a random float between 2.5 and 5.5 inclusive
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+print(random.randint(1, 100))
