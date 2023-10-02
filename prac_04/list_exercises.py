f47441be331e269d708391bc1b5406ccaa97a020
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2023-10-02 18:47:08 +1000

    Add woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,4 +1,5 @@
-"""List Exercises Practical"""
+"""
+List Exercises Practical
 
 numbers = []
 for i in range(5):
@@ -8,3 +9,17 @@ print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
+"""
+
+"""
+Woefully inadequate security checker
+"""
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Please enter you username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
