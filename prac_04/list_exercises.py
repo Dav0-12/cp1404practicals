bb44a53dcabab29d5d18685ca4b513613cb0039c
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2023-10-08 15:11:48 +1000

    Add average number functionality.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,7 @@
 """
 List Exercises Practical
+"""
+
 
 numbers = []
 for i in range(5):
@@ -9,7 +11,8 @@ print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
-"""
+print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+
 
 """
 Woefully inadequate security checker
