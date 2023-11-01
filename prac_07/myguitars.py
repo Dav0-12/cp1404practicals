bdd7c18d6d93a351e8a3762d6a6a46cd4ce994e5
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2023-11-01 16:37:27 +1000

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,14 +7,22 @@ Actual Duration:
 import csv
 from prac_07.guitar import Guitar
 
+FILE_NAME = 'guitars.csv'
+
 guitars = []
-with open('guitars.csv', 'r', newline='') as in_file:
+with open(FILE_NAME, 'r', newline='') as in_file:
     in_file.readline()
     reader = csv.reader(in_file)
     for row in reader:
         guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
 
-for guitar in sorted(guitars):
-    print(guitar)
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    price = float(input("Price: "))
+    guitars.append(Guitar(name, year, price))
+    name = input("Name: ")
 
-
+with open(FILE_NAME, 'w') as out_file:
+    for guitar in sorted(guitars):
+        print(f'{guitar.name},{guitar.year},{guitar.cost}', file=out_file)
