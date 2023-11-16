ebabc6642e3f941b867fd5867e1dae9b455aa846
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2023-11-16 15:27:13 +1000

    Complete prac 09 task 1

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,21 @@
+"""
+Prac 09 - Taxi Testing Code
+"""
+from prac_09.taxi import Taxi
+
+
+def main():
+    taxi = Taxi("Prius 1", 100)
+    taxi.drive(40)  # Drive 40 kilometres
+    print_taxi_details(taxi)
+    taxi.start_fare()  # Restart the meter (start a new fare)
+    taxi.drive(100)  # Drive 100 kilometres
+    print_taxi_details(taxi)
+
+
+def print_taxi_details(taxi):
+    print(taxi)  # Print the taxi's details
+    print(taxi.get_fare())  # Print the current fare
+
+
+main()
